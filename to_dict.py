6f7951041b525f8a00a19fc992327c1b07688541
esteban <estebanboada1994@gmail.com>
2022-07-13 10:10:37 -0500

    to_dict.py
    algoritmo que convierte una lista de listas, a una lista de diccionarios

---
+++ to_dict.py
@@ -0,0 +1,52 @@
+#convertir una lista de listas a lista de diccionarios, cambia orden de datos
+def list_to_dict(lista: list, headers: list):
+    if len(lista) > 1:
+        medio = len(lista) // 2
+        izquierda = lista[:medio]
+        derecha = lista[medio:]
+        
+        # llamada recursiva en cada mitad
+        list_to_dict(izquierda,headers)
+        list_to_dict(derecha,headers)
+        
+        # Iteradores para recorrer las dos sublistas
+        i = 0
+        j = 0
+        # Iterador para la lista principal
+        k = 0
+
+        while i < len(izquierda) and j < len(derecha):
+            if type(izquierda[i])!=dict:
+                lista[k] = dict(zip(headers,izquierda[i]))
+                i += 1
+                k += 1
+            elif type(derecha[j])!=dict:
+                lista[k] = dict(zip(headers,derecha[j]))
+                j += 1
+                k += 1
+            else:
+                lista[k] = izquierda[i]
+                lista[k+1] = derecha[j]
+                i += 1
+                j += 1
+                k += 2
+        #Escribe restante por izquierda cuando excede el index
+        while i < len(izquierda):
+            if type(izquierda[i])!=dict:
+                lista[k] = dict(zip(headers,izquierda[i]))
+                k +=1
+            else:
+                lista[k] = izquierda[i]
+                k +=1
+            i += 1
+        #Escribe restante por derecha cuando excede el index    
+        while j < len(derecha):
+            if type(derecha[j])!=dict:
+                lista[k] = dict(zip(headers,derecha[j]))
+                k += 1
+            else:
+                lista[k] = derecha[j]
+                k += 1
+            j += 1
+        
+        return lista
